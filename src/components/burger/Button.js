b57cfa0e1570bfcb46f0980f22db1e860e6d0366
tomdewildt <t.l.dewildt@gmail.com>
2019-08-27 21:11:39 +0200

    Add burger component

---
+++ src/components/burger/Button.js
@@ -0,0 +1,26 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+    display: block;
+    padding: ${ ( { theme } ) => theme.burger.padding }rem;
+    cursor: pointer;
+    transition-property: opacity, filter;
+    transition-duration: 0.15s;
+    transition-timing-function: linear;
+
+    font: inherit;
+    color: inherit;
+    text-transform: none;
+    background-color: transparent;
+    border: 0;
+    margin: 0;
+    overflow: visible;
+
+    ${ ( { breakpoint, theme } ) => breakpoint && css`
+        @media (min-width: ${ theme.breakpoints[ breakpoint ] }em) {
+            display: none;
+        }
+    ` }
+`;
+
+export default Button;
