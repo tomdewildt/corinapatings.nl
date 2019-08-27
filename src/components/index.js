d19e5b9436f1a4a585c79d1784281ddbf31edfa5
tomdewildt <t.l.dewildt@gmail.com>
2019-08-27 21:14:07 +0200

    Add component index file

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+export { default as TextArea } from "./form/fields/TextArea";
+export { default as Heading } from "./typography/Heading";
+export { default as Select } from "./form/fields/Select";
+export { default as Container } from "./grid/Container";
+export { default as Input } from "./form/fields/Input";
+export { default as Padding } from "./spacing/Padding";
+export { default as Margin } from "./spacing/Margin";
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Button } from "./button/Button";
+export { default as Text } from "./typography/Text";
+export { default as Column } from "./grid/Column";
+export { default as Group } from "./form/Group";
+export { default as Label } from "./form/Label";
+export { default as Form } from "./form/Form";
+export { default as Row } from "./grid/Row";
