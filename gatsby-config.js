c1f6f2aad335b8ac15e85d3b3f66f39237003c82
tomdewildt <t.l.dewildt@gmail.com>
2019-09-03 16:58:05 +0200

    Add site metadata & install dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,21 @@
 module.exports = {
     siteMetadata: {
-        name: "Corina Patings",
+        title: "Corina Patings",
+        description: "",
+        avatar: "https://www.corinapatings.nl/images/avatar.png",
+        avatarSmall: "https://www.corinapatings.nl/images/avatar-small.png",
+        banner: "https://www.corinapatings.nl/images/banner.png",
+        url: "https://www.corinapatings.nl",
+        facebook: "",
+        linkedin: "",
     },
     plugins: [
         "gatsby-plugin-layout",
+        "gatsby-plugin-react-helmet",
         {
             resolve: "gatsby-plugin-styled-components",
             options: {
-                displayName: false,
+                displayName: process.env.NODE_ENV !== "production",
             },
         },
         {
@@ -23,7 +31,6 @@ module.exports = {
             },
         },
         "gatsby-plugin-offline",
-
         "gatsby-plugin-netlify-cms",
         "gatsby-plugin-netlify",
     ],
