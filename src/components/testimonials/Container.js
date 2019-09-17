52ff35ff82e65825553ced22f55ebe7ff86f4a24
tomdewildt <t.l.dewildt@gmail.com>
2019-09-17 21:37:58 +0200

    Add testimonials

--- src/components/testimonials/Container.js
+++ src/components/testimonials/Container.js
@@ -1,10 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Heading from "../typography/Heading";
+import Padding from "../spacing/Padding";
+import Column from "../grid/Column";
+import Row from "../grid/Row";
+
 const TestimonialContainer = ( { children } ) => (
-    <div>
-        { children }
-    </div>
+    <>
+        <Row>
+            <Column>
+                <Padding top={ 4 } bottom={ 3 }>
+                    <Heading.H3 center>Ervaringen</Heading.H3>
+                </Padding>
+            </Column>
+        </Row>
+        <Row>
+            { children }
+        </Row>
+    </>
 );
 
 TestimonialContainer.propTypes = {
