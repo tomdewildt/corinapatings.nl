075ef1c2d76cf975f402bfb483f342b374b67a01
tomdewildt <t.l.dewildt@gmail.com>
2019-10-01 21:46:06 +0200

    Add kracht inzicht page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from "netlify-cms-app";
 
 import IndexPagePreview from "./previews/index-page";
+import ContentPagePreview from "./previews/content-page";
 
 CMS.registerPreviewTemplate( "index", IndexPagePreview );
+CMS.registerPreviewTemplate( "kracht-inzicht", ContentPagePreview );
