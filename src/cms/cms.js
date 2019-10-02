bfb0d6046b0ba3918a052a9282cea516ecd061a7
tomdewildt <t.l.dewildt@gmail.com>
2019-10-02 22:42:20 +0200

    Add CMS previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,4 +4,7 @@ import IndexPagePreview from "./previews/index-page";
 import ContentPagePreview from "./previews/content-page";
 
 CMS.registerPreviewTemplate( "index", IndexPagePreview );
-CMS.registerPreviewTemplate( "kracht-inzicht", ContentPagePreview );
+CMS.registerPreviewTemplate( "volwassenen", ContentPagePreview );
+CMS.registerPreviewTemplate( "kinderen", ContentPagePreview );
+CMS.registerPreviewTemplate( "opstellingen", ContentPagePreview );
+CMS.registerPreviewTemplate( "hooggevoeligheid", ContentPagePreview );
