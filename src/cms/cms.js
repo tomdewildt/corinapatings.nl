90e093680ba5a6ff43d8fdf47b376573b3a5046b
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2019-10-03 19:43:11 +0200

    Fix kracht-inzicht preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import IndexPagePreview from "./previews/index-page";
 import ContentPagePreview from "./previews/content-page";
 
 CMS.registerPreviewTemplate( "index", IndexPagePreview );
+CMS.registerPreviewTemplate( "kracht-inzicht", ContentPagePreview );
 CMS.registerPreviewTemplate( "volwassenen", ContentPagePreview );
 CMS.registerPreviewTemplate( "kinderen", ContentPagePreview );
 CMS.registerPreviewTemplate( "opstellingen", ContentPagePreview );
